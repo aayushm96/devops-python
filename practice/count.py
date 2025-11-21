72dff162db35f9d15d7f6a064cdb647471d94c8d
Aayush Mishra <aayushdivya@Aayushs-MacBook-Air.local>
2025-11-21 21:06:01 +0530

    structure

--- practice/count.py
+++ practice/count.py
@@ -1,2 +1,13 @@
 from collections import Counter
 print(Counter("engineering"))
+
+word = "engineering"
+char_count = {}
+
+for ch in word:
+    if ch in char_count:
+        char_count[ch] += 1
+    else:
+        char_count[ch] = 1
+
+print(char_count)
