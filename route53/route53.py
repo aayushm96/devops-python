91762b30f5ce9c734be3af205498e56b5c5eed84
Aayush Mishra <aayushdivya@Aayushs-MacBook-Air.local>
2025-11-21 22:06:11 +0530

    records

--- route53/route53.py
+++ route53/route53.py
@@ -1,57 +1,58 @@
 import boto3
-import json
 from openpyxl import Workbook
 
-client = boto3.client("route53")
+# Create Route53 client with clear name
+r53 = boto3.client("route53")
 
-zones = client.list_hosted_zones()
-
-output = []
-
-# Collect all data
-for zone in zones["HostedZones"]:
-    zone_id = zone["Id"]
-    records = client.list_resource_record_sets(HostedZoneId=zone_id)
-
-    output.append({
-        "HostedZone": zone["Name"],
-        "Records": records["ResourceRecordSets"]
-    })
-
-# ---------------------------
-# Write to Excel (.xlsx)
-# ---------------------------
-
+# ------------------------
+# Create Excel Workbook
+# ------------------------
 wb = Workbook()
 ws = wb.active
 ws.title = "Route53 Records"
 
-# Header row
+# Excel header row
 ws.append(["Hosted Zone", "Record Name", "Record Type", "TTL", "Record Value"])
 
-for zone_data in output:
-    hosted_zone = zone_data["HostedZone"]
-    
-    for record in zone_data["Records"]:
-        name = record.get("Name", "")
-        rtype = record.get("Type", "")
-        ttl = record.get("TTL", "")
+# ------------------------
+# Fetch Hosted Zones
+# ------------------------
+zones = r53.list_hosted_zones()["HostedZones"]
 
-        # Records can have multiple values
-        values = record.get("ResourceRecords", [])
-        if values:
-            for v in values:
-                ws.append([hosted_zone, name, rtype, ttl, v["Value"]])
-        else:
-            # For ALIAS records, no ResourceRecords
-            alias = record.get("AliasTarget", {})
-            if alias:
-                ws.append([hosted_zone, name, rtype, ttl, alias.get("DNSName", "")])
+# ------------------------
+# Loop zones & paginate record sets
+# ------------------------
+for zone in zones:
+    zone_id = zone["Id"]
+    zone_name = zone["Name"]
+
+    paginator = r53.get_paginator("list_resource_record_sets")
+
+    # Iterate over ALL pages
+    for page in paginator.paginate(HostedZoneId=zone_id):
+        records = page["ResourceRecordSets"]
+
+        for record in records:
+            name = record.get("Name", "")
+            rtype = record.get("Type", "")
+            ttl = record.get("TTL", "")
+
+            # A/TXT/MX records (multiple values)
+            if "ResourceRecords" in record:
+                for rr in record["ResourceRecords"]:
+                    ws.append([zone_name, name, rtype, ttl, rr["Value"]])
+
+            # Alias targets (ALB, CloudFront, etc.)
+            elif "AliasTarget" in record:
+                alias_dns = record["AliasTarget"].get("DNSName", "")
+                ws.append([zone_name, name, rtype, ttl, alias_dns])
+
+            # Records with no values (rare)
             else:
-                ws.append([hosted_zone, name, rtype, ttl, ""])
+                ws.append([zone_name, name, rtype, ttl, ""])
 
-# Save Excel file
+# Save Excel File
 excel_file = "route53_output.xlsx"
 wb.save(excel_file)
 
-print(f"Saved to {excel_file}")
+print(f"Saved all Route53 records to {excel_file} using Route53 client (r53).")
